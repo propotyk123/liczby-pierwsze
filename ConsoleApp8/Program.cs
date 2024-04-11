9ac8355d807fc8f393dc75c57070150c7e861faa
labidibka <labidibka@gmail.com>
2024-04-11 13:58:12 +0200

    liczby_pierwsze_complete

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,44 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Wypelnienie();
+    }
+    static void Wypelnienie()
+    {
+        bool[] tab = new bool[100];
+        for (int i = 0; i < tab.Length; i++)
+        {
+            tab[i] = true;
+        }
+        Pierwsze(tab);
+    }
+    static void Pierwsze(bool[] tab)
+    {
+        for (int i = 2; i < Math.Sqrt(100); i ++)
+        {
+            if (tab[i] == true)
+            {
+                for (int j = i * i; j < 100; j += i )
+                {
+                    tab[j] = false;
+                }
+            }
+        }
+        Wyswietlanie(tab);
+    }
+    static void Wyswietlanie(bool[] tab)
+    {
+        for ( int i = 3; i < tab.Length; i++)
+        {
+            if (tab[i] == true)
+            {
+                Console.Write(i + ",");
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
